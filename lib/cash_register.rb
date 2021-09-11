a161da5949902c3517f5eaaef1b43aa43fec7bf8
Kyle Ortiz <kyleaortiz@gmail.com>
2021-09-11 17:42:00 -0500

    commit tree ab789fbc0b6eb37fe9940a7c61e5f3a88a791e50

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
 
+     attr_accessor :total, :discount
+
+     def initialize(discount = 20)
+          self.total = 0
+          if discount > 0 
+               self.discount = (100 - discount)/100
+               self.apply_discount
+          end
+     end
+
+     def add_item (item, price, quantity = 1)
+          transaction_total = price * quantity
+          self.total += transaction_total
+          @last_transaction = [item, price, quantity]
+     end
+
+     def apply_discount
+          self.total = @total * @discount
+          puts "After the discount, the total comes to $#{total}."
+     end
+
+end
